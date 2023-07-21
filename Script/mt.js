a24bd6672250053d4f35781e5fe36aef1252c91b
lhsll <82965551+lhsll@users.noreply.github.com>
2023-07-21 23:49:34 +0800

    更新 mt.js

--- Script/mt.js
+++ Script/mt.js
@@ -1,1 +1,9 @@
-Modify response-body .* {"error_msg":".*","data":.*} {"error_msg":"","data":{}}
+// Surge脚本，将名为"search_faxian"的数据返回为空
+$done({
+  body: JSON.stringify({
+    "error_msg": "",
+    "data": {
+      "search_faxian": {}
+    }
+  })
+});
