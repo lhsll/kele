e0ba49aed12325a92bcae3d8800c027ef9c18e8d
lhsll <82965551+lhsll@users.noreply.github.com>
2023-07-21 23:42:02 +0800

    更新 mt.js

--- Script/mt.js
+++ Script/mt.js
@@ -1,15 +1,1 @@
-function blockAds(request, response) {
-  const responseData = JSON.parse(response.body); // 解析响应的 JSON 数据
-
-  // 遍历 entities 数组查找匹配的广告链接
-  for (const entity of responseData.entities) {
-    if (entity.title === '来来来「赚点零花钱」 >>>') {
-      response.status = 404; // 返回 404 响应以屏蔽广告链接
-      break; // 找到匹配的链接后，停止遍历
-    }
-  }
-
-  return response;
-}
-
-$done({ response: blockAds });
+Modify response-body .* {"error_msg":".*","data":.*} {"error_msg":"","data":{}}
