69a8d65555f94471093af7f14ba32460d46f4288
lhsll <82965551+lhsll@users.noreply.github.com>
2023-08-14 21:37:30 +0800

    更新 SearchArtifact.js

--- chongxie/Script/SearchArtifact.js
+++ chongxie/Script/SearchArtifact.js
@@ -1,5 +1,3 @@
-
-
 let obj = JSON.parse($response.body);
 
 obj.data.vipType = 1024,
@@ -7,7 +5,4 @@ obj.data.vipLabelLevel = 999,
 obj.data.vipLabel = "荣耀捐赠会员",
 obj.data.username = "喜欢喝可乐",
 obj.data.avatarUrl = "https://raw.githubusercontent.com/lhsll/Sta/main/Picture/touxiang.PNG",
-
-
-
 $done({body: JSON.stringify(obj)});
