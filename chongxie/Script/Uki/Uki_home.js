d76246db366b07a958f8b0a01599b73863215912
lhsll <82965551+lhsll@users.noreply.github.com>
2023-09-28 03:31:24 +0800

    更新 Uki_home.js

--- chongxie/Script/Uki/Uki_home.js
+++ chongxie/Script/Uki/Uki_home.js
@@ -1,11 +1,14 @@
 // 获取响应主体
-let 内容 = $response.内容;
+let body = $response.body;
 
-// 解析 JSON 格式的响应体为一个对象
+// 将响应主体解析为 JSON 对象
 let obj = JSON.parse(body);
 
 // 使用筛选函数过滤出 "id" 属性值为 "ZXYY" 的项
-obj.data.tab = obj.data.tab.filter(tab => tab.id === "ZXYY");
+obj.data.tabs = obj.data.tabs.filter(tab => tab.id === "ZXYY");
 
-// 将重新转换后的 JSON 字符串发送包含修改后的响应体
-$done({ 内容: JSON.stringify(obj) });
+// 将修改后的 JSON 对象转换回 JSON 字符串
+let modifiedBody = JSON.stringify(obj);
+
+// 使用 $done() 函数将修改后的响应返回到原始请求
+$done({ body: modifiedBody });
