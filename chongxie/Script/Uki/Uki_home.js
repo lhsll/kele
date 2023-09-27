0821ca2bd2f4842ce75c541fb33501d2ccaa2637
lhsll <82965551+lhsll@users.noreply.github.com>
2023-09-28 03:02:46 +0800

    Update and rename Uki_home .js to Uki_home.js

--- chongxie/Script/Uki/Uki_home.js
+++ chongxie/Script/Uki/Uki_home.js
@@ -1,11 +1,11 @@
 // 获取响应主体
-let body = $response.body;
+let 内容 = $response.内容;
 
 // 解析 JSON 格式的响应体为一个对象
 let obj = JSON.parse(body);
 
 // 使用筛选函数过滤出 "id" 属性值为 "ZXYY" 的项
-obj.data.tabs = obj.data.tabs.filter(tab => tab.id === "ZXYY");
+obj.data.tab = obj.data.tab.filter(tab => tab.id === "ZXYY");
 
 // 将重新转换后的 JSON 字符串发送包含修改后的响应体
-$done({ body: JSON.stringify(obj) });
+$done({ 内容: JSON.stringify(obj) });
