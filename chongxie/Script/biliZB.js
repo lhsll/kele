4397c33068ff6891a19aa22ac752a0976b20f5d7
lhsll <82965551+lhsll@users.noreply.github.com>
2023-10-28 22:36:53 +0800

    更新 biliZB.js

--- chongxie/Script/biliZB.js
+++ chongxie/Script/biliZB.js
@@ -1,13 +1,21 @@
+// 获取响应主体
 let body = $response.body;
-let obj = JSON.parse(body);
 
+// 解析 JSON 格式的响应体为一个对象
+let obj = JSON.parse($response.body);
+
+// 保留"data"数据组下的指定"biz_id"数据项
+let keepBizIds = [997, 998, 2, 16, 3];
 if (obj.data && obj.data.outer_list) {
-    obj.data.outer_list = obj.data.outer_list.filter(item => {
-        let title = item.title;
-        return !["魔法奇遇", "守护圣殿", "帮玩订单", "购物车", "荣耀等级"].includes(title);
-    });
+    obj.data.outer_list = obj.data.outer_list.filter(item => keepBizIds.includes(item.biz_id));
 }
 
+// 保留"interaction_list"数据组中"biz_id"为32的相关项
+if (obj.data && obj.data.interaction_list) {
+    obj.data.interaction_list = obj.data.interaction_list.filter(item => item.biz_id === 32);
+}
+
+// 去除广告
 if (obj.data) {
     obj.data.activity_banner_info = undefined;
     obj.data.shopping_info = { is_show: 0 };
